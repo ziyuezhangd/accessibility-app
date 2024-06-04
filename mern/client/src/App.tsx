4de38717edeca0ca47e1aa093735512e565a1ea3
April Polubiec <aprilpolubiec@gmail.com>
2024-06-04 19:45:10 -0400

    create and render basic map component

--- mern/client/src/App.tsx
+++ mern/client/src/App.tsx
@@ -1,15 +1,15 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Map } from './components/Map';
 
-import './App.css'
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
-
-
-  const App = () => {
-    return (
-      <div className="w-full p-6">
-        <Navbar />
-        <Outlet />
-      </div>
-    );
-  };
-  export default App
+const App = () => {
+  return (
+    <div className='w-full p-6'>
+      <Navbar />
+      <Map />
+      <Outlet />
+    </div>
+  );
+};
+export default App;
