d2f964a90894f8668d4f20241f5e4d6d9bd17130
Ellen Doherty <147493910+ellendoherty@users.noreply.github.com>
2024-06-04 09:53:13 +0100

    App set up
    * Create README
    
    * Update README
    
    * Create .gitignore
    
    * Update README
    
    * Update workflow in README
    
    * Update testing in README
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Boiler Plate basic app setup
    
    * Installed Dependencies
    
    * Dependencies
    
    * Create server.js
    
    * Config and connection files
    
    * record.js
    
    * adding components
    
    * Update .gitignore 
    
    Include .DS_Store
    
    * Update .gitignore
    
    Update rules for .env files
    
    * remove gitignore
    
    * Create .gitignore
    
    * Change recordlist.js to .jsx
    
    * stubbing out routes
    
    * Update .gitignore
    
    Ignore dependencies node_module/
    
    * remove DS STORE
    
    * Remove node_modules from tracking
    
    ---------
    
    Co-authored-by: daniellez23 <157396790+daniellez23@users.noreply.github.com>

---
+++ mern/server/routes/pointOfInterest.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+// This will help us connect to the database
+import db from "../db/connection.js";
+
+// This help convert the id from string to ObjectId for the _id.
+import { ObjectId } from "mongodb";
+
+// router is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const router = express.Router();
