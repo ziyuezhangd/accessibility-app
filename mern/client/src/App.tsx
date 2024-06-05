24be1d851e8a686efce878f85062fdef68d5da2f
April Polubiec <aprilpolubiec@gmail.com>
2024-06-05 14:58:31 +0100

    can render a map

--- mern/client/src/App.tsx
+++ mern/client/src/App.tsx
@@ -1,18 +1,26 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import { Map } from './components/Map';
+import { GoogleMapApiLoader } from 'react-google-map-wrapper';
+import { Suspense } from 'react';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+
 const App = () => {
+  const googleMapConfig: string = import.meta.env.VITE_GOOGLEMAP_KEY;
+
   return (
     <div className='w-full p-6'>
-      <Navbar />
-      <Map />
-      <Outlet />
+      {/* TODO: add fallback={<Fallback />} */}
+      <Suspense>
+        {/* Load the google map api */}
+        <GoogleMapApiLoader apiKey={googleMapConfig} suspense>
+          <Map />
+        </GoogleMapApiLoader>
+      </Suspense>
     </div>
   );
 };
