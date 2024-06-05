1c982c1dc100aa0e17c7ef395408886a8d1a13d7
April Polubiec <aprilpolubiec@gmail.com>
2024-06-05 14:54:19 +0100

    add google map key to vite env

--- mern/client/src/vite-env.d.ts
+++ mern/client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_GOOGLEMAP_KEY: string;
+  // add more environment variables here if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
