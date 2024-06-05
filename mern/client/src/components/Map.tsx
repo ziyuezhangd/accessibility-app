29c8661f10915faaa36cee54e5e2284cc16fafda
April Polubiec <aprilpolubiec@gmail.com>
2024-06-05 14:58:31 +0100

    can render a map

--- mern/client/src/components/Map.tsx
+++ mern/client/src/components/Map.tsx
@@ -1,5 +1,11 @@
+import { GoogleMap, Marker } from 'react-google-map-wrapper';
+// Docs: https://pyjun01.github.io/react-google-map-wrapper/docs/introdution/
 export const Map = () => {
   return (
-    <div>Map</div>
-  )
-}
+    // you can pass props to map container element.
+    // use Tailwind CSS or styled-components or anything to style your container.
+    <GoogleMap className='h-[400px]' zoom={17} center={{ lat: 37.5709413, lng: 126.977787 }}>
+      <Marker lat={37.5709413} lng={126.977787} />
+    </GoogleMap>
+  );
+};
