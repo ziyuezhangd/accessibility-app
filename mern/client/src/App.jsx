2ba9c0d743759199f400f4c6be89e9426cc3fa4b
April Polubiec <aprilpolubiec@gmail.com>
2024-06-06 17:08:32 +0100

    add some components and basic routing

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,28 +1,16 @@
 import './App.css';
-import { Map } from './components/Map';
-import { GoogleMapApiLoader } from 'react-google-map-wrapper';
-import { StrictMode, Suspense } from 'react';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  const googleMapConfig = import.meta.env.VITE_GOOGLEMAP_KEY;
-
   return (
-    <StrictMode>
-      <div className='w-full p-6'>
-        {/* TODO: add fallback={<Fallback />} */}
-        <Suspense>
-          {/* Load the google map api */}
-          <GoogleMapApiLoader apiKey={googleMapConfig} suspense>
-            <Map />
-          </GoogleMapApiLoader>
-        </Suspense>
-      </div>
-    </StrictMode>
+    <div className='w-full p-6'>
+      <Outlet />
+    </div>
   );
 };
 export default App;
