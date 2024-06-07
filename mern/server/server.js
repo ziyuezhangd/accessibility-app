c7298c3f0ec005210c3d7398dbbe131bb39b6d0f
Ellen Doherty <ellenmdoherty@gmail.com>
2024-06-07 12:08:42 +0100

    FIx endpoints naming

--- mern/server/server.js
+++ mern/server/server.js
@@ -6,9 +6,6 @@ import placeInfosRouter from './routes/place-infos.js';
 import busynessRating from "./routes/busynessRating.js"
 import noiseRating from "./routes/noiseRating.js"
 import odourRating from "./routes/odourRating.js"
-//import blockRatings from "./routes/blockRating.js"
-//import models from "./routes/modelRoute.js"
-//import reports from "./routes/report.js"
 import accessibilityHighlightPlace from "./routes/accessibilityHighlightPlace.js"
 
 const PORT = process.env.PORT || 5050;
@@ -21,10 +18,7 @@ app.use('/place-infos', placeInfosRouter);
 app.use("/busyness-ratings", busynessRating)
 app.use("/noise-ratings", noiseRating)
 app.use("/odour-ratings", odourRating)
-//app.use("/blockRating", blockRatings);
-//app.use("/modelRoute", models);
-//app.use("/report", reports);
-app.use("/accessibilityHighlightPlace", accessibilityHighlightPlace)
+app.use("/accessibility-highlight-place", accessibilityHighlightPlace)
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
