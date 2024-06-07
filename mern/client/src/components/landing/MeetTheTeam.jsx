8bbb2973806f99e81cc089dd19a2e02da65a7d3a
April Polubiec <aprilpolubiec@gmail.com>
2024-06-07 16:19:49 +0100

    add all landing page components

---
+++ mern/client/src/components/landing/MeetTheTeam.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MeetTheTeam() {
+  return (
+    <div>MeetTheTeam</div>
+  )
+}
--- mern/client/src/pages/Landing.jsx
+++ mern/client/src/pages/Landing.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/landing/Hero';
+import Features from '../components/landing/Features';
+import Blurb from '../components/landing/Blurb';
+import Faq from '../components/landing/Faq';
+import MeetTheTeam from '../components/landing/MeetTheTeam';
 
 export default function Landing() {
   return (
-    <div>Landing</div>
-  )
+    <>
+      <div>Landing</div>
+      <Hero />
+      <Features />
+      <Blurb />
+      <Faq />
+      <MeetTheTeam />
+    </>
+  );
 }
