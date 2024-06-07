1d34b4a751e244dd174ec9e9cf277f4f1aeccb6d
Ellen Doherty <ellenmdoherty@gmail.com>
2024-06-07 11:38:19 +0100

    feedback endppoint
    add body-parser
    remove report.js

--- mern/server/server.js
+++ mern/server/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 import path from "path";
+//npm install body-parser
+import bodyParser from "body-parser";
 import busynessRating from "./routes/busynessRating.js"
 import noiseRating from "./routes/noiseRating.js"
 import odourRating from "./routes/odourRating.js"
+import feedback from "./routes/feedback.js"
 //import blockRatings from "./routes/blockRating.js"
 //import models from "./routes/modelRoute.js"
 //import reports from "./routes/report.js"
@@ -13,16 +16,14 @@ import odourRating from "./routes/odourRating.js"
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
 app.use("/busyness-ratings", busynessRating)
 app.use("/noise-ratings", noiseRating)
 app.use("/odour-ratings", odourRating)
-//app.use("/blockRating", blockRatings);
-//app.use("/modelRoute", models);
-//app.use("/report", reports);
-//app.use("/pointOfInterest", pointsOfInterst)
+app.use("/feedback", feedback)
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
