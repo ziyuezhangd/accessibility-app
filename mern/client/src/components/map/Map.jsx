14965ded2797d7526b10d21c8d184acf3c37ead9
April Polubiec <aprilpolubiec@gmail.com>
2024-06-08 16:01:00 +0100

    move click listener to new map

--- mern/client/src/components/map/Map.jsx
+++ mern/client/src/components/map/Map.jsx
@@ -5,11 +5,26 @@ import { Box, useTheme } from '@mui/material';
 // Docs: https://pyjun01.github.io/react-google-map-wrapper/docs/introdution/
 export const Map = () => {
   const theme = useTheme();
+  const handleMapClicked = (map, e) => {
+    const isPlaceIconClicked = e.placeId !== undefined;
+    const isLocationClicked = e.placeId === undefined;
+    const latLng = e.latLng;
+    const lat = latLng.lat();
+    const lng = latLng.lng();
+    if (isPlaceIconClicked) {
+      // Do things
+      console.log('Place clicked: ', e.placeId, lat, lng);
+    }
+    if (isLocationClicked) {
+      // Do things
+      console.log('Location clicked: ', lat, lng);
+    }
+  };
   return (
     // you can pass props to map container element.
     // use Tailwind CSS or styled-components or anything to style your container.
     <Box sx={{ ...theme.mixins.toolbar, flexGrow: 1 }}>
-      <GoogleMap style={{ height: '95vh', top: '7vh' }} zoom={DEFAULT_ZOOM} center={{ lat: MANHATTAN_LAT, lng: MANHATTAN_LNG }}>
+      <GoogleMap style={{ height: '95vh', top: '7vh' }} zoom={DEFAULT_ZOOM} center={{ lat: MANHATTAN_LAT, lng: MANHATTAN_LNG }} onClick={handleMapClicked}>
         <HelpIcon />
         <Marker lat={MANHATTAN_LAT} lng={MANHATTAN_LNG} />
       </GoogleMap>
