e3887f07d1dcfa5acbe70e041155c35bfb7a9545
daniellez23 <daniellezhzy@gmail.com>
2024-06-08 18:23:05 +0100

    Contributes to ENG-86: fix eslint config for backend

--- mern/server/eslint.config.js
+++ mern/server/eslint.config.js
@@ -1,43 +1,60 @@
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+import globals from "globals";
+import importPlugin from "eslint-plugin-import";
 
 export default [
   {
-    ignorePatterns: ['logs', 'eslint.config.js', 'node_modules'],
-    env: {
-      node: true,
-      es2021: true,
+    ignores: ["logs", "eslint.config.js", "node_modules"]
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+        process: "writable",
+        __dirname: "writable",  // readonly?
+      },
     },
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: "module"
+    plugins: {
+      import: importPlugin,
     },
-    plugins: [
-      "eslint",
-      "import",
-    ],
-    extends: [
-      "eslint:recommended",
-      "plugin:import/errors", 
-      "plugin:import/warnings",
-      pluginJs.configs.recommended
-    ],
     rules: {
-      // Best Practices
-      "eqeqeq": ["error", "always"],
-      "no-multi-spaces": "error",
-
+      // Import
+      "import/no-unresolved": "error",
+      "import/named": "error",
+      "import/no-named-export": "error",
+      "import/first": "error",
+      "import/order": ["error", {
+        groups: [
+          "builtin",
+          "external",
+          "object",
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      }],
+      
       // Variables
       "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+      "eqeqeq": ["error", "always"],
 
       // Stylistic Issues
       "indent": ["error", 2],
       "quotes": ["error", "single"],  // always use single quotes
       "semi": ["error", "always"],  // always use semicolons
       "no-multiple-empty-lines": ["error", { "max": 1 }],  // at most 2 consecutive blank row
+      "no-multi-spaces": "error",
       "padding-line-between-statements": [
         "error",
-        { "blankLine": "always", "prev": "import", "next": "*", "ignoreConsecutive": true },  // 1 blank row after import
-        { "blankLine": "always", "prev": "*", "next": "export", "ignoreConsecutive": true }   // 1 blank row before export
+        { "blankLine": "never", "prev": "import", "next": "import" },  // no blank row for consecutive import statements
+        { "blankLine": "always", "prev": "import", "next": "*" },  // 1 blank row after import
+        { "blankLine": "always", "prev": "*", "next": "export" }   // 1 blank row before export
       ],
 
       // ES6
