f1b8595490e64f4d700838a2175f7c3c27577634
April Polubiec <aprilpolubiec@gmail.com>
2024-06-08 12:13:58 +0100

    add basic left drawer

--- mern/client/src/pages/MapPage.jsx
+++ mern/client/src/pages/MapPage.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Map as MapComponent } from '../components/map/Map';
 import { GoogleMapApiLoader } from 'react-google-map-wrapper';
 import { Suspense } from 'react';
+import PersistentDrawerLeft from '../components/map/Drawer';
 
 export default function MapPage() {
   const googleMapConfig = import.meta.env.VITE_GOOGLEMAP_KEY;
 
   return (
-    <Suspense>
-      {/* TODO: add fallback */}
-      {/* Load the google map api */}
-      <GoogleMapApiLoader apiKey={googleMapConfig} suspense>
-        <MapComponent />
-      </GoogleMapApiLoader>
-    </Suspense>
+    <>
+      <PersistentDrawerLeft />
+      <Suspense>
+        {/* TODO: add fallback */}
+        {/* Load the google map api */}
+        <GoogleMapApiLoader apiKey={googleMapConfig} suspense>
+          <MapComponent />
+        </GoogleMapApiLoader>
+      </Suspense>
+    </>
   );
 }
