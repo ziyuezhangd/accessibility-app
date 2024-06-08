bedb78fc8f17c800eddc2c3074e2c14d05bbc6c4
daniellez23 <daniellezhzy@gmail.com>
2024-06-08 23:17:21 +0100

    Contributes to ENG-86: fix frontend eslint

--- mern/client/eslint.config.js
+++ mern/client/eslint.config.js
@@ -1,35 +1,28 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import pluginImport from 'eslint-plugin-import';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default [
   {
-    ignorePatterns: ['dist', 'eslint.config.js', 'node_modules'],
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 12,
+    ignores: ['dist', 'eslint.config.js', 'node_modules']
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
       sourceType: "module",
-      ecmaFeatures: { jsx: true },
-    }, 
-    plugins: [
-      "react",
-      "react-hooks",
-      "import",
-      "jsx-a11y",
-    ],
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:react-hooks/recommended",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "plugin:jsx-a11y/recommended",
-      "airbnb",
-      "plugin:@typescript-eslint/recommended",
-      pluginJs.configs.recommended,
-    ],
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
     settings: {
       react: {
         version: "detect",
@@ -44,44 +37,73 @@ export default [
         },
       },
     },
+    plugins: {
+      "import": pluginImport,
+      "react-hooks": pluginReactHooks,
+      "jsx-a11y": pluginJsxA11y,
+      "react-refresh": reactRefresh,
+    },
     rules: {
-      // Best Practices
-      "eqeqeq": ["error", "always"],
-      "no-multi-spaces": "error",
+      // Import
+      "import/no-unresolved": "error",
+      "import/named": "error",
+      "import/no-named-export": "error",
+      "import/first": "error",
+      "import/order": ["error", {
+        groups: [
+          "builtin",
+          "external",
+          "object",
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      }],
 
       // Variables
       "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+      "eqeqeq": ["error", "always"],
 
       // Stylistic Issues
       "indent": ["error", 2],
-      "quotes": ["error", "single"], // always use single quotes
-      "semi": ["error", "always"], // always use semicolons
-      "no-multiple-empty-lines": ["error", { "max": 1 }], // at most 1 consecutive blank row
+      "quotes": ["error", "single"],  // always use single quotes
+      "semi": ["error", "always"],  // always use semicolons
+      "no-multiple-empty-lines": ["error", { "max": 1 }],  // at most 2 consecutive blank row
+      "no-multi-spaces": "error",
       "padding-line-between-statements": [
         "error",
-        { "blankLine": "always", "prev": "import", "next": "*", "ignoreConsecutive": true }, // 1 blank row after import
-        { "blankLine": "always", "prev": "*", "next": "export", "ignoreConsecutive": true }, // 1 blank row before export
+        { "blankLine": "never", "prev": "import", "next": "import" },  // no blank row for consecutive import statements
+        { "blankLine": "always", "prev": "import", "next": "*" },  // 1 blank row after import
+        { "blankLine": "always", "prev": "*", "next": "export" }   // 1 blank row before export
       ],
+
+      // ES6
+      "prefer-const": ["error", {
+        "destructuring": "all",
+        "ignoreReadBeforeAssign": true
+      }],
+      "arrow-spacing": ["error", { "before": true, "after": true }],
 
       // React
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
-      'react-refresh/only-export-components': ["warn", { allowConstantExport: true }],
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
 
       // React Hooks
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
 
       // Accessibility
-      'jsx-a11y/anchor-is-valid': 'warn',
-
-      // ES6
-      "prefer-const": ["error", {
-        "destructuring": "all",
-        "ignoreReadBeforeAssign": true,
-      }],
-      "arrow-spacing": ["error", { "before": true, "after": true }],
+      "jsx-a11y/anchor-is-valid": "warn",
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/label-has-associated-control": "warn",
+      "jsx-a11y/no-autofocus": "warn",
+      "jsx-a11y/no-redundant-roles": "warn",
+      "jsx-a11y/interactive-supports-focus": "warn",
     },
-  }
+  },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
 ];
