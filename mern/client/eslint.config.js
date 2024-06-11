1b46936deb2086b7a8eb641f92b0f47404206f7a
daniellez23 <daniellezhzy@gmail.com>
2024-06-12 00:54:00 +0100

    test with dir_names_include_files option

--- mern/client/eslint.config.js
+++ mern/client/eslint.config.js
@@ -66,7 +66,7 @@ export default [
         },
       }],
 
-      // Best Practice - test1
+      // Best Practice - test
       "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
       "eqeqeq": ["error", "always"],
 
--- mern/server/eslint.config.js
+++ mern/server/eslint.config.js
@@ -39,7 +39,7 @@ export default [
         },
       }],
       
-      // Best Practice - test
+      // Best Practice - test1
       "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
       "eqeqeq": ["error", "always"],
       'no-console': 'warn',
