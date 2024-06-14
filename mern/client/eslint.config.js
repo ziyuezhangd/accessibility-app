5e7638fb876b33c66de8f5b7eb397f586e634f23
Danielle Zhang <157396790+daniellez23@users.noreply.github.com>
2024-06-14 16:30:17 +0100

    Ziyuezhang/eng 99 configure GitHub actions for eslint (#23)
    * Contributes to ENG-99: configure eslint.yml
    
    * Contributes to ENG-99: set up github workflow
    
    * Adjust dependencies versions
    
    * Contributes to ENG-99: separates frontend check from backend check
    
    * test frontend
    
    * debug eslint.yml
    
    * test with tj-actions/changed-files@v44
    
    * test with dir_names option
    
    * test with files option
    
    * test with files option 2
    
    * test with dir_names_include_files option
    
    * test with ifs and outputs
    
    * test with max warning
    
    * debug workflow status
    
    * change names
    
    * test for frontend
    
    * test for backend
    
    * debug in eslint config for server
    
    * debug in eslint config for server
    
    * update gitignore and package.json for server
    
    * remove --fix

--- mern/client/eslint.config.js
+++ mern/client/eslint.config.js
@@ -66,7 +66,7 @@ export default [
         },
       }],
 
-      // Variables
+      // Best Practice
       "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
       "eqeqeq": ["error", "always"],
 
