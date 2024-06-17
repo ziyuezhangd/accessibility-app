596fe0956f51b95127dec357d5b0d847399edcfb
April Polubiec <aprilpolubiec@gmail.com>
2024-06-17 14:02:21 +0100

    can call feedback post endpoint

--- mern/client/src/components/map/Drawer.jsx
+++ mern/client/src/components/map/Drawer.jsx
@@ -32,7 +32,7 @@ export default function PersistentDrawerLeft() {
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          ['& .MuiDrawer-paper']: { width: drawerWidth, boxSizing: 'border-box' },
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
         }}
       >
         <Toolbar />
--- mern/client/src/services/feedback.js
+++ mern/client/src/services/feedback.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-named-export */
 const dummyFeedbacks = [
   {
     name: 'April',
@@ -13,6 +14,17 @@ const dummyFeedbacks = [
   },
 ];
 
-export const postFeedback = (feedback) => {
+export const postFeedback = async (feedback) => {
   // Call the API here
+  const response = await fetch('http://localhost:5050/feedback', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(feedback),
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error);
+  }
 };
