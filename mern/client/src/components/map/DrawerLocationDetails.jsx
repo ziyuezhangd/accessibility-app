6295b2879539214aaadd718ef818d7abda7cd030
April Polubiec <aprilpolubiec@gmail.com>
2024-06-17 14:02:21 +0100

    can call feedback post endpoint

--- mern/client/src/components/map/DrawerLocationDetails.jsx
+++ mern/client/src/components/map/DrawerLocationDetails.jsx
@@ -1,9 +1,11 @@
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { Button, Alert } from '@mui/material';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import { postFeedback } from '../../services/feedback';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -15,6 +17,19 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 
 export default function DrawerLocationDetails({ location, onBackClicked }) {
+  const [error, setError] = React.useState('');
+  const handleButtonClicked = async () => {
+    try {
+      await postFeedback({
+        name: 'April',
+        email: 'april.polubiec@ucdconnect.ie',
+        comment: 'This place is no longer open.',
+        coordinates: [-73.9712, 40.7831],
+      });
+    } catch (e) {
+      setError(e.toString());
+    }
+  };
   return (
     <>
       <DrawerHeader>
@@ -25,7 +40,9 @@ export default function DrawerLocationDetails({ location, onBackClicked }) {
       <Box sx={{ overflow: 'auto' }}>
         <Typography variant='h5'>{location}</Typography>
         Hello
+        <Button onClick={handleButtonClicked}>Submit Feedback</Button>
       </Box>
+      {error && <Alert severity='error'>{error}</Alert>}
     </>
   );
 }
