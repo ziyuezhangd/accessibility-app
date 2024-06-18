4776a278efcdfd00174992e231d48a998b8fcb52
daniellez23 <daniellezhzy@gmail.com>
2024-06-18 14:54:01 +0100

    remove record.js

--- mern/server/routes/index.js
+++ mern/server/routes/index.js
@@ -5,7 +5,6 @@ import feedback from './feedback.js';
 import noiseRating from './noiseRating.js';
 import odourRating from './odourRating.js';
 import placeInfos from './place-infos.js';
-import records from './record.js';
 import soundRating from './soundRating.js';
 
 const router = express.Router();
@@ -16,7 +15,6 @@ router.use('/feedback', feedback);
 router.use('/noise-ratings', noiseRating);
 router.use('/odour-ratings', odourRating);
 router.use('/place-infos', placeInfos);
-router.use('/record', records);
 router.use('/sound-ratings', soundRating);
 
 export default router;
