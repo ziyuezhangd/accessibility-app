d441f66939503aa0500c695181d0d9a7848ff8d1
NITHISHH SARAVANAN <nithishh.saravanan@ucdconnect.ie>
2024-06-19 17:18:24 +0100

    fixed eslint errors

--- mern/client/src/utils/MapUtils.js
+++ mern/client/src/utils/MapUtils.js
@@ -1,25 +1,26 @@
-// Existing code
+
 export const MANHATTAN_LAT = 40.7831;
+
 export const MANHATTAN_LNG = -73.9712;
+
 export const DEFAULT_ZOOM = 14;
 
-// New code for gradients
 export const busynessGradient = [
-  'rgba(0, 0, 255, 0)', // transparent blue
-  'rgba(0, 0, 255, 1)', // blue
-  'rgba(255, 0, 0, 1)'  // red
+  'rgba(0, 0, 255, 0)',// transparent blue
+  'rgba(0, 0, 255, 1)',// blue
+  'rgba(255, 0, 0, 1)'// red
 ];
 
 export const noiseGradient = [
-  'rgba(0, 255, 0, 0)', // green
+  'rgba(0, 255, 0, 0)',// green
   'rgba(0, 255, 0, 1)',
-  'rgba(255, 255, 0, 1)', // yellow
-  'rgba(255, 0, 0, 1)' // red
+  'rgba(255, 255, 0, 1)',// yellow
+  'rgba(255, 0, 0, 1)'// red
 ];
 
 export const odorGradient = [
-  'rgba(0, 255, 0, 0)', // green
+  'rgba(0, 255, 0, 0)',// green
   'rgba(0, 255, 0, 1)',
-  'rgba(255, 255, 0, 1)', // yellow
-  'rgba(128, 0, 128, 1)' // purple
+  'rgba(255, 255, 0, 1)',// yellow
+  'rgba(128, 0, 128, 1)'// purple
 ];
