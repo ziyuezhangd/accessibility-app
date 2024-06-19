27e8bc266b7ad168127ce66d2964577e1b6997eb
April Polubiec <aprilpolubiec@gmail.com>
2024-06-19 16:20:04 +0100

    put styling in landing

--- mern/client/src/components/landing/Hero.jsx
+++ mern/client/src/components/landing/Hero.jsx
@@ -19,13 +19,17 @@ export default function Hero() {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        gap: { xs: 3, sm: 6 },
+        justifyContent: 'center',
         height: '85vh',
       }}
     >
-      <Typography variant='h1'>Discover Accessible Areas in NYC</Typography>
-      <Typography variant='h2'>Find the best spots that cater to your needs</Typography>
-      <Button variant='contained' onClick={handleGetStartedClicked}>
+      <Typography variant='h1' fontFamily={'Playfair Display'}>
+        Discover Accessible Areas in NYC
+      </Typography>
+      <Typography variant='h4' component={'h2'} sx={{ mb: 4 }}>
+        Find the best spots that cater to your needs
+      </Typography>
+      <Button variant='contained' size='large' onClick={handleGetStartedClicked}>
         Get started
       </Button>
       <Button variant='text'>Learn more</Button>
