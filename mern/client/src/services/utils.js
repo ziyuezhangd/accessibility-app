dec924eb0094478a497d3135f02014b2618864a0
Danielle Zhang <157396790+daniellez23@users.noreply.github.com>
2024-06-19 16:14:00 +0100

    Ziyuezhang/eng 116 configure proxy for development (#38)
    * Contributes to ENG-116: configure proxy in package.json
    
    * modify urls in api calls

--- mern/client/src/services/utils.js
+++
@@ -1,9 +0,0 @@
-const DEV = import.meta.env.DEV;
-let API_HOST;
-if (DEV) {
-  API_HOST = import.meta.env.VITE_DEV_API_HOST;
-} else {
-  API_HOST = import.meta.env.VITE_PROD_API_HOST;
-}
-
-export { API_HOST };
