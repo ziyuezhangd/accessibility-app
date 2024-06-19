cfb35c36095c7bda4762d7ef33ae31481e3f5418
daniellez23 <daniellezhzy@gmail.com>
2024-06-19 22:56:16 +0100

    Contributes to ENG-45: test for placeInfos.js and restructure

---
+++ mern/server/routes/placeInfos.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import logger from '../logger.js';
+import accessibilityCloud from '../service/accessibilityCloud.js';
+
+const placeInfosRouter = express.Router();
+
+placeInfosRouter.get('/', async (req, res) => {
+  try {
+    const placeInfos = await accessibilityCloud.getPlaceInfos();
+    res.status(200).json(placeInfos);
+  } catch (error) {
+    logger.error(error.stack);
+    res.status(500).send({ message: 'Failed to get place infos.', error: error.message });
+  }
+});
+
+placeInfosRouter.get('/categories', async (req, res) => {
+  try {
+    const categories = await accessibilityCloud.getCategories();
+    res.status(200).send(categories);
+  } catch (error) {
+    logger.error(error.stack);
+    res.status(500).send({ message: 'Failed to get categories.', error: error.message });
+  }
+});
+
+export default placeInfosRouter;
---
+++ mern/server/tests/placeInfos.test.js
@@ -0,0 +1,60 @@
+import { describe, it, expect, jest, beforeAll, afterAll } from '@jest/globals';
+import express from 'express';
+import request from 'supertest';
+import router from '../routes/placeInfos.js';
+import accessibilityCloud from '../service/accessibilityCloud.js';
+
+const app = express();
+app.use('/', router);
+
+describe('GET', () => {
+  const dummyPlaces = [
+    {'category':'police','name':'New York City Police Headquarters','latitude':40.7119414,'longitude':-74.0020911},
+    {'category':'library','name':'Chatham Square Library','latitude':40.7133126,'longitude':-73.9963202}
+  ];
+  const dummyCategories = ['airport','biergarten','books'];
+
+  beforeAll(() => {
+    jest.mock('../service/accessibilityCloud.js');
+  });
+
+  describe('/placeInfo', () => {
+    it('should return 200 and result if external API call succeeds', async () => {
+      jest.spyOn(accessibilityCloud, 'getPlaceInfos').mockResolvedValue(dummyPlaces);
+
+      const response = await request(app).get('/');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(dummyPlaces);
+    });
+    it('should return 500 if external API call fails', async () => {
+      jest.spyOn(accessibilityCloud, 'getPlaceInfos').mockImplementation(() => {
+        throw new Error('Exteral API error');
+      });
+      
+      const response = await request(app).get('/');
+      expect(response.status).toBe(500);
+    });
+  });
+
+  describe('/placeInfo/categories', () => {
+    it('should return 200 and result if external API call succeeds', async () => {
+      jest.spyOn(accessibilityCloud, 'getCategories').mockResolvedValue(dummyCategories);
+
+      const response = await request(app).get('/categories');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(dummyCategories);
+    });
+    it('should return 500 if external API call fails', async () => {
+      jest.spyOn(accessibilityCloud, 'getCategories').mockImplementation(() => {
+        throw new Error('Exteral API error');
+      });
+
+      const response = await request(app).get('/categories');
+      expect(response.status).toBe(500);
+    });
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+});
