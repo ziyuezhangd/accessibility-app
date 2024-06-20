bf9c2e16b4ab117e9c5de6c665ef7045bdbdb5ac
April Polubiec <aprilpolubiec@gmail.com>
2024-06-20 09:01:19 +0100

    create basic nearby stations component

--- mern/client/src/utils/MapUtils.js
+++ mern/client/src/utils/MapUtils.js
@@ -1,3 +1,6 @@
+import { blue, brown, green, grey, lime, orange, purple, red, yellow } from '@mui/material/colors';
+import _ from 'lodash';
+
 export const MANHATTAN_LAT = 40.7831;
 
 export const MANHATTAN_LNG = -73.9712;
@@ -14,4 +17,52 @@ export const calculateDistanceBetweenTwoCoordinates = (lat1, lon1, lat2, lon2) =
 
   var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
   return d * 1000; // Meters
+};
+
+/**
+ * @param {string} placeInfoString - String as returned by place-infos endpoint (eg: '42nd Street-Grand Central (S,4,5,6,7)')
+ */
+export const getSubwayLinesFromPlaceInfoString = (placeInfoString) => {
+  const openingParenIdx = placeInfoString.indexOf('(');
+  const closingParenIdx = placeInfoString.indexOf(')');
+  const linesString = placeInfoString.substring(openingParenIdx + 1, closingParenIdx);
+  const linesArr = linesString.split(',');
+  return linesArr;
+};
+
+/**
+ * @param {string} placeInfoString - String as returned by place-infos endpoint (eg: '42nd Street-Grand Central (S,4,5,6,7)')
+ */
+export const getSubwayStationNameFromPlaceInfoString = (placeInfoString) => {
+  const openingParenIdx = placeInfoString.indexOf('(');
+  return placeInfoString.substring(0, openingParenIdx);
+};
+
+// https://en.wikipedia.org/wiki/List_of_New_York_City_Subway_lines
+export const SUBWAY_LINE_COLORS = {
+  A: blue[900],
+  C: blue[900],
+  E: blue[900],
+  B: orange[800],
+  D: orange[800],
+  F: orange[800],
+  M: orange[800],
+  G: lime['A700'],
+  L: grey[700],
+  J: brown[700],
+  Z: brown[700],
+  N: yellow[700],
+  Q: yellow[700],
+  R: yellow[700],
+  W: yellow[700],
+  1: red[700],
+  2: red[700],
+  3: red[700],
+  4: green[900],
+  5: green[900],
+  6: green[900],
+  7: purple[700],
+  T: blue[200],
+  S: grey[400],
+  PATH: blue[900],
 };
