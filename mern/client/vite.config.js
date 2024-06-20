b951031d9cdcf69eafecccd5ac15bd1c5436dbfd
Danielle Zhang <157396790+daniellez23@users.noreply.github.com>
2024-06-20 13:55:40 +0100

    Ziyuezhang/eng 116 configure proxy for development (#43)
    * Contributes to ENG-116: configure proxy in package.json
    
    * modify urls in api calls
    
    * configure proxy in vite

--- mern/client/vite.config.js
+++ mern/client/vite.config.js
@@ -6,4 +6,13 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5050',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
