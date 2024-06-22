2c0a4a747e99f0bf7e1f6a065cb685c8fb0151aa
Danielle Zhang <157396790+daniellez23@users.noreply.github.com>
2024-06-22 16:08:22 +0100

    Ziyuezhang/eng 45 create testing scripts for backend routes (#47)
    * Contributes to ENG-45: test for accessibilityHighlightPlace.js
    
    * debug accessibilityHighlightPlace.js
    
    * Contributes to ENG-45: test for busynessRating.js
    
    * mock database connection
    
    * Contributes to ENG-45: test for noiseRating.js
    
    * Contributes to ENG-45: test for soundRating.js
    
    * Contributes to ENG-45: test for odourRating.js
    
    * Contributes to ENG-45: test for feedback.js
    
    * fix logger issues
    
    * log error stack
    
    * Contributes to ENG-45: test for placeInfos.js and restructure
    
    * change folder name
    
    * encapsulate external api calls into cityofNY.js
    
    * debug
    
    * tests for pedestrianRamps, pedestrianSignals, seatingAreas
    
    * run lint
    
    * Address pr feedback
    
    * Add docstrings

--- mern/server/routes/pedestrianSignals.js
+++ mern/server/routes/pedestrianSignals.js
@@ -1,22 +1,15 @@
 import express from 'express';
+import cityofNY from '../apis/cityofNY.js';
 import logger from '../logger.js';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const result = await fetch(`https://data.cityofnewyork.us/resource/de3m-c5p4.json?boroname=Manhattan&$$app_token=${process.env.SOCRATA_APP_KEY}`);
-    const data = await result.json();
-    // TODO: cant get the logger to work correctly
-    // logger.log(`${data}`);
-    // Filter down to the data we care about
-    const signalInfo = data.map((d) => ({
-      latitude: d.point_y,
-      longitude: d.point_x,
-    }));
+    const signalInfo = await cityofNY.getPedestrianSignals();
     res.status(200).send(signalInfo);
   } catch (error) {
-    logger.error(error);
+    logger.error(error.stack);
     res.status(500).send({ message: 'Failed to load pedestrian signals.', error: error.message });
   }
 });
