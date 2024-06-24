f760a70492ff15fd44b2ccb039a2e00910051ddc
daniellez23 <daniellezhzy@gmail.com>
2024-06-24 13:35:26 +0100

    update dbHandler

--- mern/server/db/dbHandler.js
+++ mern/server/db/dbHandler.js
@@ -1,17 +1,21 @@
-import db from 'connection.js';
+import db from './connection.js';
+import logger from '../logger.js';
 
 const dbHandler = {
-    db: db,
+  db: db,
 
-    /**
+  /**
      * @param {string} modelName
      * @returns {Promise<Array<string>>} 
      */
-    async getLatestModel(modelName) {
+  async getLatestModel(modelName) {
+    const collection = db.collection(modelName);
+    const latestModel = await collection.findOne({}, { sort: { date: -1 } });
 
-    },
+    return latestModel;
+  },
 
-    /**
+  /**
      * @param {object} feedback
      * @param {string} feedback.name
      * @param {string} feedback.email
@@ -20,9 +24,16 @@ const dbHandler = {
      * @param {[number, number]} feedback.coordinates
      * @returns {Promise<Array<string>>} 
      */
-    async insertFeedback(feedback){
+  async insertFeedback(feedback){
+    try {
+      const collection = db.collection('feedback');
+      await collection.insertOne(feedback);
+
+    } catch(error) {
+      logger.error(error.stack);
 
     }
+  }
 };
 
 export default dbHandler;
