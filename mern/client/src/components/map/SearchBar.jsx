9e81f66692ee2303aea9d8a44d925c4c9c5ff51f
April Polubiec <aprilpolubiec@gmail.com>
2024-06-25 13:23:55 +0100

    fix search box to use places

--- mern/client/src/components/map/SearchBar.jsx
+++ mern/client/src/components/map/SearchBar.jsx
@@ -2,49 +2,54 @@
 
 import SearchIcon from '@mui/icons-material/Search';
 import { TextField, InputAdornment } from '@mui/material';
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import { Control } from 'react-google-map-wrapper';
 
 const SearchBar = ({ mapInstance, setSelectedPlace }) => {
   const searchInputRef = useRef(null);
-
+  
   useEffect(() => {
-    if (mapInstance && window.google.maps.places) {
-      const input = searchInputRef.current;
-      const searchBox = new window.google.maps.places.SearchBox(input);
+    if (mapInstance) {
+      loadPlacesSearchBox();
+    }
+  }, [mapInstance]);
 
-      mapInstance.addListener('bounds_changed', () => {
-        searchBox.setBounds(mapInstance.getBounds());
-      });
+  const loadPlacesSearchBox = async () => {
+    const PlacesService = await google.maps.importLibrary('places');  
+    const input = searchInputRef.current;
+    const searchBox = new window.google.maps.places.SearchBox(input);
 
-      searchBox.addListener('places_changed', () => {
-        const places = searchBox.getPlaces();
-        if (places.length === 0) {
+    mapInstance.addListener('bounds_changed', () => {
+      searchBox.setBounds(mapInstance.getBounds());
+    });
+
+    searchBox.addListener('places_changed', () => {
+      const places = searchBox.getPlaces();
+      if (places.length === 0) {
+        return;
+      }
+
+      const bounds = new window.google.maps.LatLngBounds();
+      places.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
           return;
         }
 
-        const bounds = new window.google.maps.LatLngBounds();
-        places.forEach((place) => {
-          if (!place.geometry || !place.geometry.location) {
-            return;
-          }
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
 
-          if (place.geometry.viewport) {
-            bounds.union(place.geometry.viewport);
-          } else {
-            bounds.extend(place.geometry.location);
-          }
-
-          setSelectedPlace({
-            id: place.place_id,
-            lat: place.geometry.location.lat(),
-            lng: place.geometry.location.lng(),
-          });
+        setSelectedPlace({
+          id: place.place_id,
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng(),
         });
-        mapInstance.fitBounds(bounds);
       });
-    }
-  }, [mapInstance]);
+      mapInstance.fitBounds(bounds);
+    });
+  };
 
   return (
     <><Control position={google.maps.ControlPosition.TOP_LEFT}></Control><TextField
