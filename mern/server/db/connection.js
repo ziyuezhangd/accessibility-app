2d37a400a881ffacf25c2503f0f7ed3311ff2436
daniellez23 <daniellezhzy@gmail.com>
2024-06-26 18:16:54 +0100

    clear comments

--- mern/server/db/connection.js
+++ mern/server/db/connection.js
@@ -47,31 +47,3 @@ const getDB = async () => {
 };
 
 export default getDB;
-
-// if (!process.env.NO_DB) {
-//   logger.info(`Connecting to db: ${uri}`);
-//   const client = new MongoClient(uri, {
-//     serverApi: {
-//       version: ServerApiVersion.v1,
-//       strict: true,
-//       deprecationErrors: true,
-//     },
-//   });
-
-//   try {
-//     // Connect the client to the server
-//     logger.info('Connecting...');
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     logger.info('Pinging...');
-//     await client.db('admin').command({ ping: 1 });
-//     logger.info('Pinged your deployment. You successfully connected to MongoDB!');
-//   } catch (err) {
-//     logger.error(`${err}`);
-//   }
-
-//   db = client.db('practicumAppDB');
-// } else {
-//   logger.info('Running without database connection');
-//   db = null;
-// }
