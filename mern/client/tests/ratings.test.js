a852eda354bad89c42b114cbfd5578c88c46113c
daniellez23 <daniellezhzy@gmail.com>
2024-06-28 18:22:47 +0100

    test for ratings

--- mern/client/tests/ratings.test.js
+++ mern/client/tests/ratings.test.js
@@ -34,3 +34,65 @@ describe('Function getBusynessRatings', () => {
     expect(console.error).toHaveBeenCalledWith('An error has occurred: API error');
   });
 });
+
+describe('Function getNoiseRatings', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  it('should fetch noise ratings correctly', async () => {
+    fetch.mockResponseOnce(JSON.stringify(dummyRatings));
+
+    const ratings = await getNoiseRatings(testDateTime);
+
+    expect(fetch).toHaveBeenCalledWith('/api/noise-ratings?' + new URLSearchParams({ datetime: testDateTime }));
+    expect(ratings).toEqual(dummyRatings);
+  });
+
+  it('should handle fetch error', async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        ok: false,
+        statusText: 'API error',
+      })
+    );
+    jest.spyOn(console, 'error');
+    
+    const ratings = await getNoiseRatings(testDateTime);
+
+    expect(fetch).toHaveBeenCalledWith('/api/noise-ratings?' + new URLSearchParams({ datetime: testDateTime }));
+    expect(ratings).toBeUndefined();
+    expect(console.error).toHaveBeenCalledWith('An error has occurred: API error');
+  });
+});
+
+describe('Function getOdourRatings', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  it('should fetch odour ratings correctly', async () => {
+    fetch.mockResponseOnce(JSON.stringify(dummyRatings));
+
+    const ratings = await getOdourRatings(testDateTime);
+
+    expect(fetch).toHaveBeenCalledWith('/api/odour-ratings?' + new URLSearchParams({ datetime: testDateTime }));
+    expect(ratings).toEqual(dummyRatings);
+  });
+
+  it('should handle fetch error', async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        ok: false,
+        statusText: 'API error',
+      })
+    );
+    jest.spyOn(console, 'error');
+    
+    const ratings = await getOdourRatings(testDateTime);
+
+    expect(fetch).toHaveBeenCalledWith('/api/odour-ratings?' + new URLSearchParams({ datetime: testDateTime }));
+    expect(ratings).toBeUndefined();
+    expect(console.error).toHaveBeenCalledWith('An error has occurred: API error');
+  });
+});
