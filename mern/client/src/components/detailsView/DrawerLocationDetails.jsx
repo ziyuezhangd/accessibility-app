2abe31d2a41b4c8c934b66515a9f145c1a0cf479
NITHISHH SARAVANAN <nithishh.saravanan@ucdconnect.ie>
2024-07-01 12:57:03 +0100

    Refactor Feedback Form into Separate Component

--- mern/client/src/components/detailsView/DrawerLocationDetails.jsx
+++ mern/client/src/components/detailsView/DrawerLocationDetails.jsx
@@ -1,13 +1,12 @@
-import {PlaceOverview} from '@googlemaps/extended-component-library/react';
+import { PlaceOverview } from '@googlemaps/extended-component-library/react';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Button, TextField, Modal, Slide, Snackbar, Box, IconButton, Typography, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Button, Box, IconButton, styled } from '@mui/material';
 import _ from 'lodash';
-import{ useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import FeedbackForm from './FeedbackForm';
 import Grades from './Grades';
 import NearestRestrooms from './NearestRestrooms';
 import NearestStations from './NearestStations';
-import { postFeedback, Feedback } from '../../services/feedback';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -17,7 +16,6 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
   justifyContent: 'start',
 }));
-
 /**
  * DrawerLocationDetails component.
  * 
@@ -43,17 +41,7 @@ const formStyle = {
 };
 
 export default function DrawerLocationDetails({ location, onBackClicked }) {
-  const [isFeedbackComplete, setIsFeedbackComplete] = useState(false);
   const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [comment, setComment] = useState('');
-  const [age, setAge] = useState('');
-  const [gender, setGender] = useState('');
-  const [conditions, setConditions] = useState('');
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState('');
-  const [errors, setErrors] = useState({});
 
   useEffect(() => {
     addLocationToHistory();
@@ -84,81 +72,7 @@ export default function DrawerLocationDetails({ location, onBackClicked }) {
     setIsFeedbackOpen(true);
   };
 
-  const handleFeedbackChange = (setter) => (event) => {
-    setter(event.target.value);
-  };
-
-  const handleNameChange = (event) => {
-    const value = event.target.value;
-    if (/^[a-zA-Z\s]*$/.test(value)) {
-      setName(value);
-    }
-  };
-
-  const handleConditionsChange = (event) => {
-    const value = event.target.value;
-    if (/^[a-zA-Z\s]*$/.test(value)) {
-      setConditions(value);
-    }
-  };
-
-  const handleEmailChange = (event) => {
-    const value = event.target.value;
-    setEmail(value);
-  };
-
-  const handleAgeChange = (event) => {
-    const value = event.target.value;
-    setAge(value);
-  };
-
-  const validateEmail = (email) => {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-  };
-
-  const handleSubmitFeedback = async () => {
-    const newErrors = {};
-    if (!validateEmail(email)) {
-      newErrors.email = 'Invalid email address';
-    }
-    if (!name) {
-      newErrors.name = 'Name is required';
-    }
-    if (!comment) {
-      newErrors.comment = 'Comment is required';
-    }
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
-      return;
-    }
-    try {
-      const feedback = new Feedback(name, email, comment, Number(age), gender, conditions, [location.lng, location.lat]);
-      await postFeedback(feedback);
-      setSnackbarMessage('Feedback submitted successfully');
-      setSnackbarOpen(true);
-      setIsFeedbackOpen(false);
-      setName('');
-      setEmail('');
-      setComment('');
-      setAge('');
-      setGender('');
-      setConditions('');
-      setIsFeedbackComplete(true);
-      setErrors({});
-    } catch (e) {
-      setErrors({ form: e.toString() });
-    }
-  };
-
-  const handleSnackbarClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setSnackbarOpen(false);
-  };
-
-  const handleCancel = () => {
+  const handleFeedbackClose = () => {
     setIsFeedbackOpen(false);
   };
 
@@ -181,103 +95,12 @@ export default function DrawerLocationDetails({ location, onBackClicked }) {
         <NearestStations 
           lat={location.lat}
           lng={location.lng} />
-          
+
         <Button onClick={handleButtonClicked}>Submit Feedback</Button>
       </Box>
-      <Modal
-        open={isFeedbackOpen}
-        onClose={handleCancel}
-        closeAfterTransition
-        sx={modalStyle}
-      >
-        <Slide direction="up"
-          in={isFeedbackOpen}
-          mountOnEnter
-          unmountOnExit>
-          <Box sx={formStyle}>
-            <Typography variant="h6"
-              component="h2"
-              align="center">Submit Feedback</Typography>
-            <TextField 
-              label="Name" 
-              value={name} 
-              onChange={handleNameChange} 
-              variant="outlined" 
-              fullWidth 
-              sx={{ mt: 2 }} 
-              error={!!errors.name}
-              helperText={errors.name}
-            />
-            <TextField 
-              label="Email" 
-              value={email} 
-              onChange={handleEmailChange} 
-              variant="outlined" 
-              fullWidth 
-              sx={{ mt: 2 }} 
-              error={!!errors.email} 
-              helperText={errors.email}
-            />
-            <TextField 
-              label="Age" 
-              value={age} 
-              onChange={handleAgeChange} 
-              variant="outlined" 
-              fullWidth 
-              sx={{ mt: 2 }} 
-              type="number"
-            />
-            <FormControl fullWidth
-              sx={{ mt: 2 }}
-              error={!!errors.gender}>
-              <InputLabel>Gender</InputLabel>
-              <Select
-                value={gender}
-                onChange={handleFeedbackChange(setGender)}
-                variant="outlined"
-                label="Gender"
-              >
-                <MenuItem value="Male">Male</MenuItem>
-                <MenuItem value="Female">Female</MenuItem>
-                <MenuItem value="Other">Other</MenuItem>
-                <MenuItem value="Prefer not to say">Prefer not to say</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField 
-              label="Conditions" 
-              value={conditions} 
-              onChange={handleConditionsChange} 
-              variant="outlined" 
-              fullWidth 
-              sx={{ mt: 2 }}
-            />
-            <TextField 
-              label="Comment" 
-              value={comment} 
-              onChange={handleFeedbackChange(setComment)} 
-              variant="outlined" 
-              multiline
-              rows={4} 
-              fullWidth 
-              sx={{ mt: 2 }} 
-              error={!!errors.comment} 
-              helperText={errors.comment}
-            />
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-              <Button variant="contained"
-                color="secondary"
-                onClick={handleCancel}>Cancel</Button>
-              <Button variant="contained"
-                color="primary"
-                onClick={handleSubmitFeedback}>Send</Button>
-            </Box>
-          </Box>
-        </Slide>
-      </Modal>
-      {isFeedbackComplete && <Snackbar open={snackbarOpen}
-        autoHideDuration={3000}
-        onClose={handleSnackbarClose}
-        message={snackbarMessage} />}
+      <FeedbackForm location={location}
+        isOpen={isFeedbackOpen}
+        onClose={handleFeedbackClose} />
     </>
   );
 }
