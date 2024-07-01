6b3fd07b28cae12a77e0b9bc7e9dcbc2a893b0fc
daniellez23 <daniellezhzy@gmail.com>
2024-07-01 18:35:13 +0100

    convert segment id to lat/lng

--- mern/server/apis/ml.js
+++ mern/server/apis/ml.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+// Get dictionary of segment ID to lat/lng
+const __dirname = path.resolve();
+const filePath = path.join(__dirname, '../../ml/output/segment_to_lat_long.json');
+const data = await fs.promises.readFile(filePath, 'utf-8');
+const dictionary = JSON.parse(data);
+// Get dictionary of MODZCTA to lat/lng
+
 const ml = {
   async getNoisePredictions(hour) {
     const response  = await fetch(`/flask-api/noise-ratings?hour=${hour}`);
@@ -5,9 +15,17 @@ const ml = {
       throw new Error(`Failed to retrieve noise ratings: ${response.statusText}`);
     }
     const predictions = await response.json();
-    // Convert segment ID to lat/lng here
 
-    return predictions;
+    // Convert segment ID to lat/lng
+    const predictionsWithLatLng = predictions.map(prediction => ({
+      location: {
+        lat: dictionary[prediction.segment_id].lat,
+        lng: dictionary[prediction.segment_id].lng,
+      },
+      prediction: prediction.prediction
+    }));
+
+    return predictionsWithLatLng;
   },
 
   async getBusynessPredictions(month, day, hour, dayOfWeek) {
@@ -16,9 +34,17 @@ const ml = {
       throw new Error(`Failed to retrieve busyness ratings: ${response.statusText}`);
     }
     const predictions = await response.json();
-    // Convert segment ID to lat/lng here
+
+    // Convert segment ID to lat/lng
+    const predictionsWithLatLng = predictions.map(prediction => ({
+      location: {
+        lat: dictionary[prediction.segment_id].lat,
+        lng: dictionary[prediction.segment_id].lng,
+      },
+      prediction: prediction.prediction
+    }));
     
-    return predictions;
+    return predictionsWithLatLng;
   },
 
   async getOdourPredictions(month, day, hour, dayOfWeek) {
