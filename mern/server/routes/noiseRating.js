a673e8a6c7a5312f0b36999a9de5ff8e4b0f8c8b
daniellez23 <daniellezhzy@gmail.com>
2024-07-05 10:12:11 +0100

    add routes to backend

--- mern/server/routes/noiseRating.js
+++ mern/server/routes/noiseRating.js
@@ -3,7 +3,7 @@ import ml from '../apis/ml.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/hourly', async (req, res) => {
   const { datetime } = req.query;
   if (!datetime) {
     return res.status(400).send({ message: 'datetime parameter is required' });
@@ -14,7 +14,25 @@ router.get('/', async (req, res) => {
   }
 
   try {
-    const predictions = await ml.getNoisePredictions(datetime);
+    const predictions = await ml.getNoisePredictionsHourly(datetime);
+    res.status(200).send(predictions);
+  } catch (error){
+    res.status(500).json({message: 'Failed to retrieve the noise rating', error: error.message });
+  }
+});
+
+router.get('/daily', async (req, res) => {
+  const { datetime } = req.query;
+  if (!datetime) {
+    return res.status(400).send({ message: 'datetime parameter is required' });
+  }
+  const formatRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/;
+  if(!formatRegex.test(datetime)) {
+    return res.status(400).send({ message: 'datetime parameter should be in ISO 8601 format (e.g., \'2024-07-01T14:30:00\')' });
+  }
+
+  try {
+    const predictions = await ml.getNoisePredictionsDaily(datetime);
     res.status(200).send(predictions);
   } catch (error){
     res.status(500).json({message: 'Failed to retrieve the noise rating', error: error.message });
