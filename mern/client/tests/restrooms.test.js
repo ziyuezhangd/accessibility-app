6028f3b1718fc0088fa5ae626a7cd294cdc6e397
daniellez23 <daniellezhzy@gmail.com>
2024-07-14 18:01:32 +0100

    debug restrooms.js

--- mern/client/tests/restrooms.test.js
+++ mern/client/tests/restrooms.test.js
@@ -264,6 +264,7 @@ Sunday: Closed`.trim();
       expect(publicRestroom.isOpen(dayjs.tz('2024-07-22 19:55:20', 'America/New_York'))).toBe(false);
       expect(publicRestroom.isOpen(dayjs.tz('2024-07-23 11:55:20', 'America/New_York'))).toBe(false);
       expect(publicRestroom.isOpen(dayjs.tz('2024-07-23 19:55:20', 'America/New_York'))).toBe(true);
+      expect(publicRestroom.isOpen(dayjs.tz('2024-07-28 15:37:53', 'America/New_York'))).toBe(false);
 
       publicRestroom.hours = 'Monday to Saturday:  8:30 am-10:00 pm; Sundays: 10:00 am-7:00 pm.';
       const isOpen4 = publicRestroom.isOpen();
